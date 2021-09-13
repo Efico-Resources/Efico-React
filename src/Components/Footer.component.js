ec2eada5c5235746f54beba373a6992406de7eb3
Harith Onigemo <onigemotosin@gmail.com>
2021-09-13 16:54:03 +0400

    Fixed footer links issue

--- src/Components/Footer.component.js
+++ src/Components/Footer.component.js
@@ -23,32 +23,32 @@ function Footer() {
             <div class="col2" style={{paddingBottom: 2 + 'em'}}>
                 <h5 class="co2">Efico</h5>
                 <ul>
-                    <li class="cont"><Link to="#">About Us</Link></li>
-                    <li class="cont"><Link to="#">Services</Link></li>
-                    <li class="cont"><Link to="#">Team</Link></li>
-                    <li class="cont"><Link to="#">Sponsorship</Link></li>
-                    <li class="cont"><Link to="#">Affiliate Programme</Link></li>
-                    <li class="cont"><Link to="#">Contact Us</Link></li>
+                    <li class="cont"><Link to="/about">About Us</Link></li>
+                    <li class="cont"><Link to="/services">Services</Link></li>
+                    <li class="cont"><Link to="/about">Team</Link></li>
+                    <li class="cont"><Link to="/services">Sponsorship</Link></li>
+                    <li class="cont"><Link to="/services">Affiliate Programme</Link></li>
+                    <li class="cont"><Link to="/contact-us">Contact Us</Link></li>
                 </ul>
             </div>
             <div class="col3" style={{paddingBottom: 2 + 'em'}}>
                 <h5 class="co3">Resources</h5>
                 <ul>
-                    <li class="cont"><Link to="#">Blog</Link></li>
-                    <li class="cont"><Link to="#">Training</Link></li>
+                    <li class="cont"><Link to="/blog">Blog</Link></li>
+                    <li class="cont"><Link to="/services">Training</Link></li>
                     <li class="cont"><Link to="/faqs">FAQs</Link></li>
                 </ul>
             </div>
         </div>
         <div class="con">
             <div class="child2">
-                <span><Link to="https://www.facebook.com/resources.efico"><i class="fab fa-facebook-square"></i></Link></span>
-                <span><Link to="https://www.instagram.com/p/CSxVJtojf5c/?utm_medium=copy_link"><i class="fab fa-instagram"></i></Link></span>
-                <span><Link to="https://vm.tiktok.com/ZMRHv2mFq/"><i class="fab fa-tiktok"></i></Link></span>
-                <span><Link to="https://anchor.fm/eficoradio/episodes"><i class="fas fa-podcast"></i></Link></span>
-                <span><Link to="https://twitter.com/EficoResources"><i class="fab fa-twitter"></i></Link></span>
-                <span><Link to="https://youtube.com/channel/UC4BCMxcqIsr_ZBBafiAWV4g"><i class="fab fa-youtube"></i></Link></span>
-                <span><Link to="https://www.linkedin.com/company/efico-resources"><i class="fab fa-linkedin"></i></Link></span>
+                <span><a href="https://www.facebook.com/resources.efico" target="_blank" rel="noreferrer"><i class="fab fa-facebook-square"></i></a></span>
+                <span><a href="https://www.instagram.com/p/CSxVJtojf5c/?utm_medium=copy_link" target="_blank" rel="noreferrer"><i class="fab fa-instagram"></i></a></span>
+                <span><a href="https://vm.tiktok.com/ZMRHv2mFq/" target="_blank" rel="noreferrer"><i class="fab fa-tiktok"></i></a></span>
+                <span><a href="https://anchor.fm/eficoradio/episodes" target="_blank" rel="noreferrer"><i class="fas fa-podcast"></i></a></span>
+                <span><a href="https://twitter.com/EficoResources" target="_blank" rel="noreferrer"><i class="fab fa-twitter"></i></a></span>
+                <span><a href="https://youtube.com/channel/UC4BCMxcqIsr_ZBBafiAWV4g" target="_blank" rel="noreferrer"><i class="fab fa-youtube"></i></a></span>
+                <span><a href="https://www.linkedin.com/company/efico-resources" target="_blank" rel="noreferrer"><i class="fab fa-linkedin"></i></a></span>
                 
             </div>
             <div class="child1">
